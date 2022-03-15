8e804245ad79659d333457bc028702bd4898f361
jnash1994 <bigearblack@gmail.com>
2022-03-15 11:47:51 +0800

    java and kotlin variable, data type

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -2,10 +2,15 @@ package com.tom;
 
 public class Hello {
     public static void main(String[] args) {
-    new Person().hello();
-        for(int i=0;i<10;i++){
+        new Person().hello();
+        for(int i=0;i<50;i++){
             System.out.println(i);
         }
+
+        float weight=66.5f;
+        Integer age2=19;
+
+
     }
 
 }
